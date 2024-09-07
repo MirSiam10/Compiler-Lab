bef6e24dc26299c4e51e991d7dc9c5fdf205d15b
Mir Siam <151392892+MirSiam10@users.noreply.github.com>
2024-09-08 00:14:50 +0600

    Update p2.c

--- Compiler-Lab/p2.c
+++ Compiler-Lab/p2.c
@@ -1,18 +1,6 @@
 #Write a C Program that will count the number of white spaces in a string.
 
-#include<stdio.h>
-int main(){
-char s1[100];
-int i=0,k,count=0,space=0;
-printf("Please Enter a string:");
-gets(s1);
-for(k=0;s1[k]!='\0';k++)
-if (s1[k]==' ')
-space++;
-printf("No of space in the string:%d\n",space);
-return 0;
 
-}
 #include<stdio.h>
 int main(){
 char s1[100];
